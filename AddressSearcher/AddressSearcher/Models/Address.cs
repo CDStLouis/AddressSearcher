ce7f9164e64116f85fe01c385c88c11ea93cd563
CDStLouis <c.st.louis@outlook.com>
2020-06-22 11:41:57 +0100

    Added Address model

---
+++ AddressSearcher/AddressSearcher/Models/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AddressSearcher.Models
+{
+    public class Address
+    {
+        public string AddressName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public int Latitude { get; set; }
+        public int Longitude { get; set; }
+    }
+}
